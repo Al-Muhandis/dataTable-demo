496d4348d19a27d5e4d510029597638a3625c96f
Renat Suleymanov <mail@renat.su>
2023-11-30 18:51:51 +0400

    Add visual button to HTML

--- html_public/js/datatables-ajax.js
+++ html_public/js/datatables-ajax.js
@@ -5,5 +5,24 @@ $(document).ready(function() {
     },
     processing: true,
     serverSide: true    
+  });
+  $('#updatebtn').click(function() { 
+  
+       table = $('#datatablesSimple').dataTable({
+           destroy: true,
+           ajax: {
+             url : '/ajax.json/'
+           },
+           processing: true,
+           serverSide: true          
+       });
+       table.ajax.reload();
   }); 
+  $('#clearbtn').click(function() {
+       table = $('#datatablesSimple').DataTable({
+           destroy: true,
+           serverSide: false          
+       });
+       table.clear().draw();
+    }); 
 });
