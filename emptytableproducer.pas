3bcccd9018dc5db3c89197b5bf9921d7b6a0d91f
Renat Suleymanov <mail@renat.su>
2023-11-30 21:49:59 +0400

    Update emptytableproducer.pas

--- emptytableproducer.pas
+++ emptytableproducer.pas
@@ -17,11 +17,11 @@ uses
 { TEmptyTableProducer }
 
 procedure RouteDataTableWithAjaxPage({%H-}ARequest: TRequest; AResponse: TResponse);
-begin
+begin                { HTML template file. Create HTML page without table rows }
   AResponse.Content:=BuildHTMLTable('tpl_with-ajax.html', False); // False - without table body. Ajax will do it
 end;
 
-initialization          
+initialization
   { Routing tables at http://127.0.0.1:8081/tables-with-ajax.html }
   httprouter.RegisterRoute('/table-ajax.html', @RouteDataTableWithAjaxPage);
 
